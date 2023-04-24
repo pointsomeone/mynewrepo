ec59afb5471fe146068733b5933150c95fdea202
pointsomeone <127751046+pointsomeone@users.noreply.github.com>
2023-04-24 13:26:09 +0530

    python code for adding numbers
    python code for adding numbers

---
+++ mypyprog5.py
@@ -0,0 +1,7 @@
+#Python program to input 4 numbers ad add them
+a=int(input("enter any number"))
+b=int(input("enter any second number"))
+c=int(input("enter any third number"))
+d=int(input("enter any fourth number"))
+e=a+b+c+d
+print("addition:",e)
